fed72ee9e41c743793d3352f9470c7db47e52e48
ANDERAAM <48502025+ANDERAAM@users.noreply.github.com>
2019-03-16 15:05:13 -0500

    cuarta version
    11111

--- guardar.cpp
+++ guardar.cpp
@@ -30,7 +30,7 @@ LlenarVector(){
     }
     }
     Archivo.close();
-}kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+}
 //Modificar()
 void gotoxy(int x,int y){
       HANDLE hcon;
@@ -40,10 +40,11 @@ void gotoxy(int x,int y){
       dwPos.Y= y;
       SetConsoleCursorPosition(hcon,dwPos);
  }
+
 int main(){
 LlenarVector();
 string a,b,c,d,e;
-a=Base[4];b=Base[5];c=Base[6];d=Base[4];e=Base[5];string x;string y;
+/*a=Base[4];b=Base[5];c=Base[6];d=Base[4];e=Base[5]*/;string x;string y;
 //cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<endl;
  gotoxy(65,8); cout<<"I N G R E S E  VA L O R E S  A  L A  N O M I N A ";
  gotoxy(71,11); cout<<"1. DEDUCCIONES     (DESCUENTOS)";
@@ -56,7 +57,7 @@ gotoxy(66,11); cout<<"1. PENSION CODIGO : A || $ 50000";
 gotoxy(66,14); cout<<"2. COTIZACION DE SALUD COD : B || $ 68000";
 gotoxy(66,17); cout<<"3. PRESTAMO  CODIGO : C || $ 90000";
 gotoxy(66,20); cout<<"4. SEGURO    CODIGO : D || $ 78950";
-gotoxy(73,15); cout<<"Ingrese La Opcion Que Desee : ";cin>>y;
+gotoxy(71,23); cout<<"Ingrese La Opcion Que Desee : ";cin>>y;
 if(y=="1"){y="A50000";}
 if(y=="2"){y="B68000";}
 if(y=="3"){y="C90000";}
@@ -67,12 +68,12 @@ gotoxy(60,8); cout<<" A G R E G U E  E L  D E T A L L E  D E  B O N I F I C A C 
 gotoxy(66,12); cout<<"1. AUXILIO TRANSPORTE CODIGO :   A1 || $ 30000";
 gotoxy(66,15); cout<<"2. AUXILIO DE COMIDA  CODIGO : B1 || $ 25000";
 gotoxy(66,18); cout<<"3. PREMIO  CODIGO : C1 || $40000";
-gotoxy(73,15); cout<<"Ingrese La Opcion Que Desee : ";cin>>y;
-if(y=="1"){y="A50000";}
-if(y=="2"){y="B68000";}
-if(y=="3"){y="C90000";}
+gotoxy(71,21); cout<<"Ingrese La Opcion Que Desee : ";cin>>y;
+if(y=="1"){y="A130000";}
+if(y=="2"){y="B125000";}
+if(y=="3"){y="C140000";}
 }
-jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
+
 
 
 
